a5fd0dc1701873f9e7385eab2f80009f7d59b971
Abir Hossen <abir.cse4@gmail.com>
2024-10-07 11:56:21 +0600

    ES

---
+++ ES-6-P/defolt.js
@@ -0,0 +1,24 @@
+
+// defolt value
+function add (num1 = 0, num2 = 0){
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    
+    return result;
+}
+const sum = add(10, 5);
+// const sum = add(10);
+
+
+const a = 10;
+const b = 20;
+const result5 = 'the sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result5);
+const math = `the sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
+
+const gan = `amar sonar
+bangla
+ami 
+tomake`;
+console.log(gan);
