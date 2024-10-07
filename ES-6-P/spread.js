a4086b785a2856ebb90d9731206cd2b3c339778f
Abir Hossen <abir.cse4@gmail.com>
2024-10-07 13:11:57 +0600

    spread operator

--- ES-6-P/spread.js
+++ ES-6-P/spread.js
@@ -8,3 +8,17 @@ console.log(arrayMax);
 console.log(Numbers);
 console.log(...Numbers);
 
+
+
+// use speard operator to copy
+const friend = [45, 55, 65, 25, 12];
+const bondhu = [...friend];
+bondhu.push(13);
+
+console.log(friend);
+console.log(bondhu);
+
+// advance
+const num = [...friend, 121];
+console.log(num);
+
