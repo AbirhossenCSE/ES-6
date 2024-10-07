92f1f7e96f852a3cbbfc2fb25388c27b90311b7f
Abir Hossen <abir.cse4@gmail.com>
2024-10-07 12:51:27 +0600

    more arrow

--- ES-6-P/arrow.js
+++ ES-6-P/arrow.js
@@ -32,3 +32,35 @@ console.log(additioon);
 const multiply = (a, b) => a * b;
 const mult = multiply(5, 5);
 console.log(mult);
+
+
+
+// Single perameter
+const getAge = (person) => person.age;
+const student = { name: 'abir', age: 22};
+const age = getAge(student);
+console.log(age);
+
+
+const getThird = Numbers => Numbers[2];
+const third = getThird([5, 55, 44, 66]);
+console.log(third);
+
+
+
+// No perameter
+const getPI = () => Math.PI;
+console.log(getPI());
+
+
+
+// large arrow function
+const doMath = (x, y, z) => {
+    const sum = x + y + z;
+    const mult = x * y * z;
+    const result = sum + mult;
+    return result;
+}
+
+const xz = doMath(5, 6, 3);
+console.log(xz);
