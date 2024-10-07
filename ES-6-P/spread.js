626b572811f06d2bee5850157cd613ea1c548841
Abir Hossen <abir.cse4@gmail.com>
2024-10-07 13:02:15 +0600

    max

---
+++ ES-6-P/spread.js
@@ -0,0 +1,10 @@
+const max = Math.max(45, 65, 2, 6, 87, 89);
+console.log(max);
+
+
+const Numbers = [45, 54, 36, 12, 65, 100];
+const arrayMax = Math.max(...Numbers);
+console.log(arrayMax);
+console.log(Numbers);
+console.log(...Numbers);
+
