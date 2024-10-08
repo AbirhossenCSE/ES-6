294bcce43b5179315afe7f82a1ee2d04b5c680ab
Abir Hossen <abir.cse4@gmail.com>
2024-10-08 12:46:51 +0600

    Map

--- ES-6-P/map.js
+++ ES-6-P/map.js
@@ -1,29 +1,29 @@
-// const numbers = [4, 5, 6, 7, 8];
+const numbers = [4, 5, 6, 7, 8];
 
-// function doubleIt(num) {
-//     console.log('Num', num);
+function doubleIt(num) {
+    console.log('Num', num);
     
-//     return num * 2;
+    return num * 2;
 
-// }
+}
 
-// const result = numbers.map(doubleIt);
-// console.log(result);
-
-
-// // Advance Short tecnique
-// const double2 = n => n * 2;
-
-// const output = numbers.map(double2);
-// console.log('The double output', output);
-
-// // Another
-// const output2 = numbers.map(n => n * 2);
-// console.log(output2);
+const result = numbers.map(doubleIt);
+console.log(result);
 
 
-// const fiveadd = numbers.map(n => n + 5);
-// console.log(fiveadd);
+// Advance Short tecnique
+const double2 = n => n * 2;
+
+const output = numbers.map(double2);
+console.log('The double output', output);
+
+// Another
+const output2 = numbers.map(n => n * 2);
+console.log(output2);
+
+
+const fiveadd = numbers.map(n => n + 5);
+console.log(fiveadd);
 
 
 
