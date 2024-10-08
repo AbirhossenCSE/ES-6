02be341c2702104d5623253e40444fc14e9bacc8
Abir Hossen <abir.cse4@gmail.com>
2024-10-08 15:05:37 +0600

    class

--- ES-6-P/class.js
+++ ES-6-P/class.js
@@ -1,27 +1,27 @@
-// const products = [
-//     {id: 1, name: 'Lenovo', price: 65000},
-//     {id: 2, name: 'dell', price: 60000},
-//     {id: 3, name: 'hp', price: 40000},
-//     {id: 4, name: 'mac', price: 165000}
-// ];
+const products = [
+    {id: 1, name: 'Lenovo', price: 65000},
+    {id: 2, name: 'dell', price: 60000},
+    {id: 3, name: 'hp', price: 40000},
+    {id: 4, name: 'mac', price: 165000}
+];
 
-// // Has some properties, Method
-// class product{
-//     country = 'Bangladesh';
+// Has some properties, Method
+class product{
+    country = 'Bangladesh';
 
-//     constructor(name){
-//         this.name = name;
-//     }
+    constructor(name){
+        this.name = name;
+    }
 
-//     speak(talk){
-//         console.log(`taking about ${talk}`);
+    speak(talk){
+        console.log(`taking about ${talk}`);
         
-//     }
-// }
+    }
+}
 
-// const lenevo = new product( 'lele lanovo');
-// console.log(lenevo);
-// lenevo.speak('This is lenovo')
+const lenevo = new product( 'lele lanovo');
+console.log(lenevo);
+lenevo.speak('This is lenovo')
 
 
 
