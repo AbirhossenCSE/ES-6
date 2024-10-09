b813ee7775c55fe331efaebf02781a0b196bb2b9
Abir Hossen <abir.cse4@gmail.com>
2024-10-09 12:36:20 +0600

    closure

--- Common-JS/scope.js
+++ Common-JS/scope.js
@@ -1,8 +1,25 @@
-const x = true;
-if (x) {
-    console.log('VAlue of x is truthy');
-    
-} else {
-    console.log('value of x is falsy');
+function add(a, b){
+    const total = a + b;
+    console.log(a, b);
+    return total;
     
 }
+const result = add(4, 5);
+console.log(result);
+
+
+Closure
+
+function kitchen(){
+    let roast = 0;
+    return function(){
+        roast++;
+        return roast;
+
+    }
+}
+const firstServer = kitchen();
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
