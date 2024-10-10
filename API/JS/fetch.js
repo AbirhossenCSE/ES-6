4f691eb77118ae978db8d6e9024fcc7e26ff070e
Abir Hossen <abir.cse4@gmail.com>
2024-10-10 11:52:35 +0600

    JSON

--- API/JS/fetch.js
+++ API/JS/fetch.js
@@ -1,17 +1,17 @@
-// First tecnique
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+// // First tecnique
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
 
-      .then(response => response.json())
-    //   .then(response => console.log(response))
+//       .then(response => response.json())
+//     //   .then(response => console.log(response))
 
-      .then(json => console.log(json))
+//       .then(json => console.log(json))
 
 
 // 2nd tecnique
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
-fetch(url)
-.then(response => response.json())
-.then(json => console.log(json))
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
 
 
 // 3rd tecnique
