8baf69d3ee8cab06d14003671c63524d9520d12d
Abir Hossen <abir.cse4@gmail.com>
2024-10-10 10:52:40 +0600

    stringify

---
+++ API/JS/api.js
@@ -0,0 +1,29 @@
+const user = {id: 3, name: 'Abir', job: 'Student'};
+
+const stringified = JSON.stringify(user);
+console.log(stringified);
+console.log(user);
+
+
+const shop = {
+    owner: 'Jack Sparrow',
+    address: {
+        street: 'Mollapara',
+        city: 'Dhaka',
+        Country: 'Bangladesh'
+    },
+    product: ['laptop', 'Mic', 'Monitor', 'Keyboard'],
+    ervinue: 45000,
+    isOpen: true,
+    isNew: false
+}
+
+console.log(shop);
+
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
+
+
