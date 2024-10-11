7965877a1513c4c616a1f368a7f5d4856c5d95d9
Abir Hossen <abir.cse4@gmail.com>
2024-10-11 11:27:04 +0600

    js

--- API/contex.js
+++ API/contex.js
@@ -6,3 +6,19 @@ function add(num1, num2){
 
 const sum = add(4, 5);
 console.log(sum);
+
+
+console.log(1);
+console.log(2);
+
+doSomething();
+setTimeout(doSomething, 4000);
+
+console.log(4);
+console.log(5);
+
+
+function doSomething(){
+    console.log(3);
+    
+}
