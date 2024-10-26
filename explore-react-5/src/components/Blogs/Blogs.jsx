84fc37dc28fcf6865248e279bdf12a7b6403e9b7
Abir Hossen <abir.cse4@gmail.com>
2024-10-26 12:24:52 +0600

    react

--- explore-react-5/src/components/Blogs/Blogs.jsx
+++ explore-react-5/src/components/Blogs/Blogs.jsx
@@ -11,8 +11,8 @@ const Blogs = () => {
         .then(data => setBlogs(data))
     }, [])
     return (
-        <div>
-            <h2>Abir Hossen</h2>
+        <div className="md:w-2/3">
+            <h2>Blogs: {blogs.length} </h2>
         </div>
     );
 };
