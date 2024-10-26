cba637fcb387c51afc0ee0f095db83f8d2f238ae
Abir Hossen <abir.cse4@gmail.com>
2024-10-26 13:15:24 +0600

    handleAddToBookmark

--- explore-react-5/src/components/Blog/Blog.jsx
+++ explore-react-5/src/components/Blog/Blog.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, handleAddToBookmark }) => {
     const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
+        <div className='mb-5 p-5'>
             <img src={cover} alt={`Cover picture of ${title}`} />
-            <div className='flex justify-between'>
+            <div className='flex justify-between p-2'>
                 <div className='flex gap-3'>
-                    <img className='w-14 rounded-full' src={author_img} alt="" />
+                    <img className='w-14 rounded-full object-cover' src={author_img} alt="" />
                     <div>
                         <h3>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
                 </div>
                 <div>
-                    <span>{reading_time} min read</span>
+                    <span>{reading_time} min read </span>
+                    <button onClick={handleAddToBookmark}> <FaBookmark></FaBookmark> </button>
                 </div>
             </div>
            <h2 className='text-4xl'>{title}</h2> 
--- explore-react-5/src/components/Blogs/Blogs.jsx
+++ explore-react-5/src/components/Blogs/Blogs.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark}) => {
 
     const [blogs, setBlogs] = useState([]);
 
@@ -17,7 +17,8 @@ const Blogs = () => {
             {
                 blogs.map(blog => <Blog 
                     key={blog.id} 
-                    blog={blog}></Blog>)
+                    blog={blog}
+                    handleAddToBookmark={handleAddToBookmark}></Blog>)
             }
         </div>
     );
