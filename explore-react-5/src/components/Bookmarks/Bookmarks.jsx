64b6e8289a6701f7cf1b0c1aa6fa5583ba57ae99
Abir Hossen <abir.cse4@gmail.com>
2024-10-27 15:26:12 +0600

    done

--- explore-react-5/src/components/Bookmarks/Bookmarks.jsx
+++ explore-react-5/src/components/Bookmarks/Bookmarks.jsx
@@ -4,7 +4,7 @@ const Bookmarks = ({bookmarks, readingTime}) => {
     return (
         <div className="md:w-1/3 bg-slate-200 ml-3">
             <h2 className="text-2xl">Reading Time: {readingTime}</h2>
-           <h2 className="text-2xl">Bookmarks: {bookmarks.length} </h2> 
+           <h2 className="text-2xl"> All Bookmarks: {bookmarks.length} </h2> 
            {
                 bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
            }
